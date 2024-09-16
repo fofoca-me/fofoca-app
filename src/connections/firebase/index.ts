7d2c327ec0b1f13c5edaca5a608086688f3bdde9
Poowerllz <safilhoelzevirsafilho@gmail.com>
2024-09-16 10:56:33 -0300

    feat: setup ap

---
+++ src/connections/firebase/index.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR-AUTH-DOMAIN",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "YOUR_STORAGE-PROJECT",
+  messagingSenderId: "YOUR MESSENGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const authentication = getAuth(app);
+export { firebase, authentication };
